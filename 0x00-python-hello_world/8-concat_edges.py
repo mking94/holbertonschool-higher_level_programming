9bb9660f23394cd2827d2c1626703c4720bdc901
mking94 <dadimaher94@gmail.com>
2021-09-06 13:09:09 +0100

    More string handling with python

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str1 = str[str.index("object"):str.index("programming") + 11] + " "
-str = str1 + str[str.index("with"):str.index("with")+4] + " " + str[:6]
+str = str[39:67] + str[107:112] + str[0:6]
 print(str)
