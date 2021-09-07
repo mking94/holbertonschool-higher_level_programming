e7a87d8a83312582f31cca97af3b3c4f4b5dfdd6
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2021-09-07 21:40:26 +0100

    Function print an input string in UPPERCASE

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-def islower(c):
-    if ord(c) in range(97, 123):
-        return bool(1)
-    else:
-        return bool(0)
 def uppercase(str):
     while str != "":
-        if islower(str[0]):
+        if ord(str[0]) in range(97, 123):
             print("{:c}".format(ord(str[0])-32), end="")
         else:
             print(str[0], end="")
         str = str[1:]
+
