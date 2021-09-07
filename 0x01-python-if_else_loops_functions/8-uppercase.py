d7cafdbf96cae2a298fccfc3054669e9299bce98
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2021-09-07 21:53:17 +0100

    Print the input string in UPPERCASE

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    while str != "":
-        if ord(str[0]) in range(97, 123):
-            print("{:c}".format(ord(str[0])-32), end="")
+    ch = ""
+    for c in str:
+        if 97 <= ord(c) <= 122:
+            ch += chr(ord(c) - 32)
         else:
-            print(str[0], end="")
-        str = str[1:]
-
+            ch += c
+    print("{}".format(ch))
