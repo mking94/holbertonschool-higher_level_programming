7671f67bf374fd4f7ac7b067bb3631121286768b
mking94 <dadimaher94@gmail.com>
2021-09-13 05:46:23 -0700

    Function delete 'c' from input string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,8 +2,8 @@
 def no_c(my_string):
     liste = []
     j = 0
-    for i in range(0,len(ch)):
+    for i in range(0, len(ch)):
         if my_string[i] != 'c':
-            liste.insert(j,my_string[i])
+            liste.insert(j, my_string[i])
             j += 1
     return (''.join(liste))
