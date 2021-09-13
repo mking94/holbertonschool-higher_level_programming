951467c560086e1eb74acacc128eb17aaf57cb14
mking94 <dadimaher94@gmail.com>
2021-09-13 05:28:46 -0700

    Function that print integer of a list in reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,10 +2,10 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-  """Print in reverse."""
-  if my_list is None:
-    return (None)
-  i = len(my_list) - 1
-  while i >= 0:
-    print("{:d}".format(my_list[i]))
-    i -= 1
+    """Print in reverse."""
+    if my_list is None:
+        return (None)
+    i = len(my_list) - 1
+    while i >= 0:
+        print("{:d}".format(my_list[i]))
+        i -= 1
