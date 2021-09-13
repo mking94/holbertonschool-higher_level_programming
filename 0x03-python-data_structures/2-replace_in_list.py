29ac436af926707bf43281ff620815ccb83d10fe
mking94 <dadimaher94@gmail.com>
2021-09-13 05:33:35 -0700

    Function replace element of list at specific indx

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
+    if my_list is None:
+        return (None)
     if(idx < 0):
         return (my_list)
     elif(idx > len(my_list)):
