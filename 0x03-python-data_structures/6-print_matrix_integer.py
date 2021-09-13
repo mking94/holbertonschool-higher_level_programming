0a63f7286e5070b8281629114f5bf325ac606692
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2021-09-13 20:21:59 +0100

    Fuction print matrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if(matrix is None):
-        return (None)
-    for i in range(0, len(matrix)):
-        for j in range(0, len(matrix[i])):
-            if j == (len(matrix[i]) - 1):
-                print("{:d}".format(matrix[i][j]))
-            else:
-                print("{:d}".format(matrix[i][j]), "")
+    for i in range(len(matrix)):
+        for x in range(len(matrix[i])):
+            if x != 0:
+                print(" ", end="")
+            print("{:d}".format(matrix[i][x]), end="")
+        print()
