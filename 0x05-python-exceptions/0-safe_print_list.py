863ec7d368738b21b2f0863b35808158288a305f
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2021-09-27 04:50:44 -0400

    Function that prints x elements of a list.

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        print((' '.join(map(str, my_list[0:x]))).replace(" ",""))
+        return(x)
+    except :
+        print("Index outside of length")
