284ff47a2e4845567ccb617a37c470392696c36e
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2021-09-27 07:45:36 -0400

    Function that prints an integer with format()

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,4 +4,4 @@ def safe_print_list(my_list=[], x=0):
         print((' '.join(map(str, my_list[0:x]))).replace(" ", ""))
         return(x)
     except BaseException:
-        print("Index outside of length")
+        print("")
