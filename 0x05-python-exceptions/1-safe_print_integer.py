1207643c1d6d1c82b878d7d216a77f5b172c27db
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2021-09-27 06:29:39 -0400

    Function that prints an integer with format()

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,6 +2,8 @@
 def safe_print_integer(value):
     try:
         value += 1
-        print("{}".format(value - 1))
+        print("{:d}".format(value - 1))
+        return(bool(1))
     except BaseException:
         print("{} is not an integer".format(value))
+        return(bool(0))
