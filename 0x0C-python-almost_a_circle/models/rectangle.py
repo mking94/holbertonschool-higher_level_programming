54064572b98dd4b56610fff0ae6c63ea258de2d0
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2021-10-18 09:20:35 +0100

    Create Rectangle class that inherits from Base

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python3
+"""This module create Rectangle class that inherits from Base"""
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+    Rectangle class
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializes instance of Rectangle class"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    def display(self):
+        """dispaly the rectangle"""
+        for row in range(self.__y):
+            print()
+        for row in range(self.__height):
+            for column in range(self.__x):
+                print(" ", end="")
+            for column in range(self.__width):
+                print("#", end="")
+            print()
+
+    def __str__(self):
+        """Returns the informal representation of the object"""
+        return"[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".format(
+            self.id, self.__x, self.__y, self.__width, self.__height)
+    def update(self, *args, **kwargs):
+        """update attributes"""
+        attrs = ["id", "width", "height", "x", "y"]
+        count = len(args)
+
+        if not args or count < 1:
+            for key, value in kwargs.items():
+                for i in range(len(attrs)):
+                    if key == attrs[i]:
+                        setattr(self, attrs[i], value)
+        elif args:
+            for i in range(count):
+                setattr(self, attrs[i], args[i])
+
+    def area(self):
+        """Returns the area"""
+        return self.__width * self.__height
+
+    @property
+    def width(self):
+        """Returns width of instance"""
+        return self.__width * self.__height
+
+    @width.setter
+    def width(self, value):
+        """Sets width of class instance"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Returns height of instance"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets height of class instance"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """Returns x of instance"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Sets x of class instance"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """Returns y of instance"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Sets y of class instance"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
