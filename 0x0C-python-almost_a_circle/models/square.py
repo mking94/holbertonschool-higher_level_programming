4b10af236ee78f3e46d1424dc1fb2eec6ea6f463
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2021-10-18 15:24:19 +0100

    Square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,42 +1,41 @@
 #!/usr/bin/python3
-"""Square class inherits from rectangle"""
-
-
+"""Defines a square class."""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """
-    A class named Square
-    Attributes:
-        attr1(id): id of object
-        attr2(width): square width
-        attr3(height): square height
-        attr4(x): number of spaces before square
-        attr5(y): number of spaces before square
-    """
-    def __init__(self, size, x=0, y=0, id=None):
-        """instance of Square class"""
-        super().__init__(size, size, x, y, id)
+    """Represent a square."""
 
-    def __str__(self):
-        """string representation of Square class"""
-        return"[Square] ({:d}) {:d}/{:d} - {:d}".format(
-            self.id, self.x, self.y, self.width)
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new Square.
+            x (int): The x coordinate of the new Square.
+            y (int): The y coordinate of the new Square.
+            id (int): The identity of the new Square.
+        """
+        super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        """Returns the size of the object"""
+        """Get/set the size of the Square."""
         return self.width
 
     @size.setter
     def size(self, value):
-        """Sets the size of the object"""
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
-        """Update attributes"""
+        """Update the Square.
+        Args:
+            *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents size attribute
+                - 3rd argument represents x attribute
+                - 4th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
+        """
         if args and len(args) != 0:
             a = 0
             for arg in args:
@@ -75,3 +74,7 @@ class Square(Rectangle):
             "x": self.x,
             "y": self.y
         }
+
+    def __str__(self):
+        """Return the print() and str() representation of a Square."""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
