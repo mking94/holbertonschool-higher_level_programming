10e887ee118dc1c03748ff06a95ea2c28a1502fc
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2021-11-16 16:10:12 +0100

    Fill table with values

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
--- Invert value in table
-INSERT INTO `first_table` (`id`,`name`) VALUES(89,"Best School");
+-- Insert value in table
+INSERT INTO `first_table` (`id`,`name`) VALUES(89, "Best School");
