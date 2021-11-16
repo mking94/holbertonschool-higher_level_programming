48ee66ea4272b5df1544be13b53b5d7af1cb5ba4
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2021-11-16 12:48:41 +0100

    Delete database

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,6 +1,4 @@
 -- Deletes the database hbtn_0c_0
-IF EXISTS (SELECT name FROM master.dbo.sysdatabases WHERE name = ’hbtn_0c_0’)
-BEGIN
-DROP DATABASE `hbtn_0c_0`;
-END
+DROP DATABASE IF EXISTS `hbtn_0c_0`;
+
 
