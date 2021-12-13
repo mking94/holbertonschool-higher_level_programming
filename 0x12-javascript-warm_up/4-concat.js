c672cb6ed5f5caf419a6240e52b283dcd4131bf9
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2021-12-13 20:13:08 +0100

    Concat between two arguments

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const args1 = process.argv[2];
-const args2 = process.argv[3];
-console.log(args1 + " is " + args2);
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+console.log(arg1 + " is " + arg2);
