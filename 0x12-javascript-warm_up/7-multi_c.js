c4dc17ba211ee3b877b456f2fead790037af5879
mking94 <dadimaher94@gmail.com>
2022-04-04 05:18:37 -0700

    Script prints x timex 'C is fun'

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const times = process.argv[2];
-if(isNaN(times))
-{ 
-console.log("Missing number of occurrences");
-process.exit(1);
+if (isNaN(times)) {
+  console.log('Missing number of occurrences');
+  process.exit(1);
 }
 for (let i = 0; i < times; i++) {
-console.log("C is fun");
+  console.log('C is fun');
 }
