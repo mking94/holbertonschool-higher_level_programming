86451cccf73f2a004c8d19c38511ec3eef453753
mking94 <dadimaher94@gmail.com>
2022-04-04 04:43:49 -0700

    Script that print a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+let size = process.argv[2];
+if(isNaN(size))
+{ 
+console.log("Missing size");
+return false;
+}
+let line = new Array(Number(size)).fill("X").join('');
+for (let i = 0; i < size; i++) {
+console.log(line);
+}
+
