b6bcb1c606502ffe3e625d86ac742f32dc6c872b
mking94 <dadimaher94@gmail.com>
2022-04-04 06:24:58 -0700

    Script that searches the second biggest integer in the list of arguments.

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,8 +4,7 @@ if (tab.length <= 3) {
   console.log('0');
   process.exit(1);
 }
-tab.sort(function (a, b){
-return a - b ;
+tab.sort(function (a, b) {
+  return a - b;
 });
 console.log(tab[tab.length - 2]);
-
