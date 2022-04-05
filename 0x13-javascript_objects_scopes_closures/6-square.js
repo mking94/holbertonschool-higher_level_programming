0a336d072408dbc67b29b435cd9b7d2c475b99e8
mking94 <dadimaher94@gmail.com>
2022-04-05 14:45:24 -0700

    Define a Square class and method that inherits from Rectangle.

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const square = require('./5-square');
-module.exports = class Square extends square {
+const square1 = require('./5-square');
+module.exports = class Square extends square1 {
   charPrint (c) {
     const line = new Array(Number(this.width)).fill((c || 'X'));
     for (let i = 0; i < this.height; i++) {
