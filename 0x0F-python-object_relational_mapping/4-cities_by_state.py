ede7146534ce1e3030b806f9bb706e98af1e66b9
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-04-09 21:10:12 +0200

    Script that lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,14 +10,7 @@ if __name__ == '__main__':
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
-<< << << < HEAD
-cur.execute("SELECT cities.id, cities.name, states.name" +
-            " FROM cities LEFT JOIN states" +
-            " ON states.id = cities.state_id" +
-            " ORDER BY cities.id ASC")
-== == == =
-cur.execute("SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id = cities.state_id ORDER BY cities.id ASC")
->>>>>> > 49ff0847ff9303fd559069aa38e32e123ef53c03
-rows = cur.fetchall()
-for row in rows:
-    print(row)
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id = cities.state_id ORDER BY cities.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
