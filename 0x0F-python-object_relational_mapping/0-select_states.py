b01446cedf42b4c6fa94e87b459b574379836a4e
mking94 <dadimaher94@gmail.com>
2022-04-09 09:31:10 +0100

    Script that lists all states from the database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python
 import sys
 import MySQLdb
-if __name__ == "__nanme__":
-    db_connection= MySQLdb.connect("127.0.0.1",sys.arv[1],sys.arv[2],sys.arv[3])
+if __name__ == "__main__":
+    try:
+        db_connection = MySQLdb.connect(
+            host='127.0.0.1',
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3])
+        cursor = db_connection.cursor()
+        cursor.execute("SELECT * from `states`;")
+        liste = cursor.fetchall()
+        print(*liste, sep="\n")
+        db_connection.close()
+    except Exception as e:
+        print(e)
