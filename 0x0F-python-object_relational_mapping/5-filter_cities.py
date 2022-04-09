a440ba1bbac65c042982792433c10c6ce3c6b76f
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-04-09 21:51:32 +0200

    Script that lists all cities from the database where the satet is defined

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
 
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name\
-                FROM cities JOIN states\
+                FROM cities LEFT JOIN states\
                 ON states.id = cities.state_id\
                 Where states.name = %s\
                 ORDER BY cities.id ASC", (sys.argv[4], ))
