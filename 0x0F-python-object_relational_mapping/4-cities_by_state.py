827f63772e9cb0e0f28c4a414825fd31054bec89
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-04-09 21:17:08 +0200

    Script that lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""lists all cities from the database hbtn_0e_4_usa """
+"""
+lists all cities from the database hbtn_0e_4_usa
+"""
 
 if __name__ == '__main__':
 
@@ -17,4 +19,3 @@ if __name__ == '__main__':
     rows = cur.fetchall()
     for row in rows:
         print(row)
-    
