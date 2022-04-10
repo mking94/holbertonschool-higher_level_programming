cda473a1d26557c2e80482151ffacc84b3ed0d7e
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-04-10 21:31:04 +0200

    Prints all City objects from the database

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""prints all City objects
-from the database hbtn_0e_14_usa"""
+"""
+prints all City objects
+from the database hbtn_0e_14_usa
+"""
 
 if __name__ == "__main__":
 
