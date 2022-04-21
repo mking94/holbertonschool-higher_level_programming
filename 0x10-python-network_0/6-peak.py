12ca018f71b392aa2137ea87fb4d1746f1195d09
mking94 <dadimaher94@gmail.com>
2022-04-21 07:38:00 -0700

    Function that return the peak in a list of integer

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ Function that return the peak in a list of integer"""
+
+
 def find_peak(list_of_integers):
     """Return a peak in a list of unsorted integers."""
     if list_of_integers == []:
