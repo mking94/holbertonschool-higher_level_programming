4a10013729b7f838272cc41a284fff035b225e8f
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-04-21 16:00:25 +0200

    Script that send variables with post method using curl

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send variables with post method using curl
-curl -s -X POST --data {"email":"test@gmail.com","subject":"I will always be here for PLD"} "$1"
+curl -s -X POST -d "email=test@gmail.com&&subject=I will always be here for PLD" "$1"
