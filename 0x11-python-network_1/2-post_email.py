45dd243d812bce1760dcea994524e5d6d4d58118
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-04-22 13:35:02 +0200

    Python script that send data to URL using post method

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,10 +5,9 @@ import urllib.request as req
 import urllib.parse as p
 import sys
 
-if len(sys.argv) != 3:
-    print("Check the argument")
-else:
-    d = p.urlencode({"email": sys.argv[2]}).encode("ascii")
-    rq = req.Request(sys.argv[1], d)
-    with request.urlopen(rq) as res:
+if __name__ == "__main__":
+    data = urllib.parse.urlencode({"email": sys.argv[2]}).encode("ascii")
+
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as res:
         print(res.read().decode("utf-8"))
