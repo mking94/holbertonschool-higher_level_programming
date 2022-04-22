d6647e05d65b5324ff0c1f237be4319483fb9aae
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-04-22 11:42:39 +0200

    Python script that print a header attribute from an URL

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Python script that print a header attribute from an URL """
+
+import urllib.request as req
+import sys
+
+if len(sys.argv) != 2:
+	print("Check the argument")
+else:
+	with req.urlopen(url = sys.argv[1]) as r:
+		print(r.getheader("X-Request-Id"))
