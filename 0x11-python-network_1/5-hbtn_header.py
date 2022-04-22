6c3b36e776d28c2238a91d8cc6bd08c49a54f67c
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-04-22 14:29:29 +0200

    Python script that send request to an URL and print specific header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Python script that send request to an URL and print specific header """
+
+import requests
+import sys
+
+if len(sys.argv) != 2:
+	print("Check the argument")
+else:
+	res = requests.get(url=sys.argv[1])
+	print(res.headers['X-Request-Id'])
