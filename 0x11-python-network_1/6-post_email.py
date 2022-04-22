502345e86d06ab17adf04516c175de7d5ce3b52d
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-04-22 14:46:20 +0200

    Python script that send data to URL and print the response

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Python script that send data to URL and print the response """
+
+import requests
+import sys
+
+if len(sys.argv) != 2:
+	print("Check the argument")
+else:
+	d = {"email": sys.argv[1]}
+	res = requests.post(url=sys.argv[1],d)
+	print(res.text)
