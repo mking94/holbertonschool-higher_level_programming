9865f8110ea6df0f8e363b6a4f6d5a79246cd05b
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-04-22 14:13:24 +0200

    Python script that send request to an URL and print response

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Python script that send request to an URL and print response """
+
+import requests
+import sys
+
+if len(sys.argv) != 2:
+	print("Check the argument")
+else:
+	res = requests.get(url=sys.argv[1])
+	print("Body response:")
+	print("\t- type: {}".format(type(res.text)))
+	print("\t- content: {}".format(res.text))
