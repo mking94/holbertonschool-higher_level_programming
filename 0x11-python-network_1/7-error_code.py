a7eea9d57723153aef01e078e8c0a6b52ee4c402
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-04-22 14:59:04 +0200

    Python script that send data to URL and print the response code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Python script that send data to URL and print the response code """
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    res = requests.post(url=sys.argv[1])
+    if res.status_code == 200:
+        print(res.text)
+    else if res.status_code >= 400:
+        print("Error code: {}".format(res.status_code))
