8bd252f55348b08ceb93be5870d4dc134a3fa8be
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-04-22 13:52:23 +0200

    Python script that send request to an URL and print httpcode

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,14 +2,14 @@
 """ Python script that send request to an URL and print httpcode """
 
 import urllib.request as req
-import urllib.parse as p
+import urllib.error as err
 import sys
 
 if len(sys.argv) != 2:
     print("Check the argument")
 else:
-    if req.urlopen(sys.argv[1]).getcode() == 200:
+    try:
         with req.urlopen(sys.argv[1]) as r:
             print(r.read().decode("utf-8"))
-    else:
-        print("Error code: {}".format(req.urlopen(sys.argv[1]).getcode()))
+    except err.HTTPError as e:
+        print("Error code: {}".format(e.code))
