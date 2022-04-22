06603bb0c9e091a504e3ba0f07c357dd5864b5ed
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-04-22 16:08:15 +0200

    Python script that takes 2 arguments and ists the 10 most recent commits on a given GitHub repository.

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,16 +2,17 @@
 """
 Python script that takes 2 arguments and ists the 10 most
 recent commits on a given GitHub repository.
+Usage: ./100-github_commits.py <repository name> <repository owner>
 """
 import sys
 import requests
 
 
 if __name__ == "__main__":
-    link = "https://api.github.com/repos/{}/{}/commits".format(
+    url = "https://api.github.com/repos/{}/{}/commits".format(
         sys.argv[2], sys.argv[1])
 
-    req = requests.get(link)
+    r = requests.get(url)
     commits = r.json()
     try:
         for i in range(10):
