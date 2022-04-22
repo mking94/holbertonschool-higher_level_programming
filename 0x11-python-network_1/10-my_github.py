bb5a50e9165fe8c52ca492e7c0e1d828d76605be
mking94 <dadimaher94@gmail.com>
2022-04-22 06:58:00 -0700

    Python script that takes your GitHub credentials (username and password) and uses the GitHub API to display your id

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,7 +7,7 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": n
     acc = HTTPBasicAuth(sys.argv[1], sys.argv[2])
     r = requests.get("https://api.github.com/user", auth=acc)
     print(r.json().get("id"))
