4917fb89f38f3136d7a61a28d1b328647478365e
mking94 <dadimaher94@gmail.com>
2022-05-17 10:22:09 -0700

    JS script that gets the contents of a webpage and stores it in a file.

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const axios = require('axios');
+const fs = require('fs');
+axios({
+  method: 'GET',
+  url:  process.argv[2]
+}).then(res => {
+  if (res) {
+    let content = res.data;
+    fs.writeFile(process.argv[3], content, err => {
+      if (err) {
+        console.error(err);
+      }
+    });
+  }
+});
