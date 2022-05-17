36cba045210f54b8b458b3e8ba96aa2b141e128f
mking94 <dadimaher94@gmail.com>
2022-05-17 04:48:46 -0700

    JS script that reads and prints the content of a file.

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-var path = process.argv[2];
-var fs = require('fs');
-fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
-    if (!err) {
-        console.log(data);
-    } else {
-        console.log(err);
-    }
+const filePath = process.argv[2];
+const fs = require('fs');
+fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
+  if (!err) {
+    console.log(data);
+  } else {
+    console.log(err);
+  }
 });
