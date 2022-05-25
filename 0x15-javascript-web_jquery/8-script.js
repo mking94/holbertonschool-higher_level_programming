f21a7af698bcec3a394435509d26e577e95ac1a3
mking94 <dadimaher94@gmail.com>
2022-05-25 10:40:36 -0700

    JavaScript script that fetches and lists the title for all movies

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,5 +1,4 @@
-$.getJSON('https://swapi-api.hbtn.io/api/films/?format=json', function(data) {
-var res = data.results;
-for(i = 0; i < res.length; i++)
-$('UL#list_movies').append('<li>'+res[i].title+'</li>');
+$.getJSON('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+  const res = data.results;
+  for (i = 0; i < res.length; i++) { $('UL#list_movies').append('<li>' + res[i].title + '</li>'); }
 });
